186913098d716f94c6eb101c6a5b25a817af946a
kazuma-naka <kazuma.naka.01@gmail.com>
2024-07-12 10:32:49 -0400

    add fizzbuzz.js

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,16 @@
+for (let i = 1; i < 21; i++){
+    switch(true){
+        case i % 3 === 0 && i % 5 === 0:
+            console.log('FizzBuzz');
+            break;
+        case i % 3 === 0:
+            console.log('Fizz');
+            break;
+        case i % 5 === 0:
+            console.log('Buzz');
+            break;
+        default:
+            console.log(`${i}`);
+            break;
+    }
+}
