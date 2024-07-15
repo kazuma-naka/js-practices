b56a675e3d35dbd54c0eaf4ca4c2b1bb059d3d09
kazuma-naka <kazuma.naka.01@gmail.com>
2024-07-15 00:28:13 -0400

    switch 文から for 文に変更

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,16 +1,11 @@
 for (let i = 1; i < 21; i++){
-    switch(true){
-        case i % 3 === 0 && i % 5 === 0:
-            console.log('FizzBuzz');
-            break;
-        case i % 3 === 0:
-            console.log('Fizz');
-            break;
-        case i % 5 === 0:
-            console.log('Buzz');
-            break;
-        default:
-            console.log(`${i}`);
-            break;
-    }
+  if(i % 3 === 0 && i % 5 === 0){
+    console.log('FizzBuzz');
+  }else if(i % 3 === 0){
+    console.log('Fizz');
+  } else if(i % 5 === 0){
+    console.log('Buzz');
+  }else {
+    console.log(i.toString());
+  }
 }
