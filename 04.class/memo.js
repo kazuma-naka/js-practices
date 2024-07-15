0ce5d02c12bbfab1f613f0b1f070543ff055addc
kazuma-naka <kazuma.naka.01@gmail.com>
2024-07-15 10:40:39 -0400

    cli で複数行の文字列を取得するため、readline を import する。eof が入力されると終了する

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,1 +1,25 @@
 #!/usr/bin/env node
+
+import readline from "readline";
+
+function addingMemo() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let inputLines = [];
+
+  console.log("メモの本文を入力してください");
+  console.log("eof で入力を終了");
+
+  rl.on("line", (input) => {
+    if (input.trim().toUpperCase() === "EOF") {
+      rl.close();
+    } else {
+      inputLines.push(input);
+    }
+  });
+
+  rl.on("close", () => {});
+}
