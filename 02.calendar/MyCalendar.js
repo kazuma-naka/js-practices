587220497557492da9dc9c39794d446f71356bd0
kazuma-naka <kazuma.naka.01@gmail.com>
2024-07-15 09:33:00 -0400

    初めの週の日付を表示するメソッドと残りの日付を表示するメソッドが別々であったのでそれを一つにまとめる。それに伴い必要でないメソッドを削除。

--- 02.calendar/MyCalendar.js
+++ 02.calendar/MyCalendar.js
@@ -29,35 +29,20 @@ class MyCalendar {
 
   #displayDatesOfMonth(month, year) {
     const startOfMonth = DateTime.local(year, month, 1);
-    const endOfMonth = startOfMonth.endOf("month");
-    const firstWeekDay = startOfMonth.weekday;
-    const allDatesInMonth = this.#getAllDatesInMonth(startOfMonth, endOfMonth);
-    this.#displayFirstWeek(firstWeekDay);
-    this.#displayRestOfDates(year, month, firstWeekDay, allDatesInMonth);
+    const endOfMonth = startOfMonth.endOf("month").day;
+    this.#displayRestOfDates(year, month, endOfMonth);
   }
 
-  #displayFirstWeek(firstWeekDay) {
-    const startIndex = this.#getStartIndexForFirstWeekDay(firstWeekDay);
-    let dates = " ";
-    for (let i = 0; i < startIndex; i++) {
-      dates += "   ";
-    }
-    for (let i = 1; i <= 7 - startIndex; i++) {
-      dates += " " + i + " ";
-    }
-    console.log(dates);
-  }
-
-  #displayRestOfDates(year, month, firstWeekDay, allDatesInMonth) {
-    const numberOfDatesInFirstWeek =
-      this.#getNumberOfDatesInFirstWeek(firstWeekDay);
+  #displayRestOfDates(year, month, endOfMonth) {
     let daysInString = "";
-    for (
-      let date = numberOfDatesInFirstWeek;
-      date < allDatesInMonth.length + 1;
-      date++
-    ) {
+    for (let date = 1; date <= endOfMonth; date++) {
       const weekDay = DateTime.local(year, month, date).weekday;
+      if (date === 1) {
+        const startIndex = weekDay % 7;
+        for (let i = 0; i < startIndex; i++) {
+          daysInString += "   ";
+        }
+      }
       daysInString = daysInString + " " + date.toString().padStart(2, " ");
       if (weekDay === 6) {
         daysInString = daysInString + "\n";
@@ -66,39 +51,10 @@ class MyCalendar {
     console.log(daysInString);
   }
 
-  #getAllDatesInMonth(startOfMonth, endOfMonth) {
-    let current = startOfMonth;
-    const dates = [];
-    while (current <= endOfMonth) {
-      dates.push({
-        date: current.toISODate(),
-        dayOfWeek: current.weekdayLong,
-      });
-      current = current.plus({ days: 1 });
-    }
-    return dates;
-  }
-
   #getMonthName(monthNumber) {
     const date = new Date();
     date.setMonth(monthNumber - 1);
     return new Intl.DateTimeFormat("ja-JP", { month: "long" }).format(date);
-  }
-
-  #getStartIndexForFirstWeekDay(firstWeekDay) {
-    if (firstWeekDay === 7) {
-      return 0;
-    } else {
-      return firstWeekDay;
-    }
-  }
-
-  #getNumberOfDatesInFirstWeek(firstWeekDay) {
-    if (firstWeekDay === 7) {
-      return firstWeekDay + 1;
-    } else {
-      return 8 - firstWeekDay;
-    }
   }
 }
 
