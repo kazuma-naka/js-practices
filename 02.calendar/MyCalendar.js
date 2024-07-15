2b076edeadc1a1109ad4b749eb7fd08aa441d5d0
kazuma-naka <kazuma.naka.01@gmail.com>
2024-07-15 01:42:59 -0400

    初めの週を表示するメソッドを luxon の weekday を使用してシンプルにする

--- 02.calendar/MyCalendar.js
+++ 02.calendar/MyCalendar.js
@@ -25,56 +25,21 @@ class MyCalendar {
   }
 
   #displayDatesOfMonth(month, year) {
-    const dateStartOfMonth = this.#getDatesOfMonth(month, year)[0].dayOfWeek;
-    let datesOfMonthInString = "";
-    for (
-      let i = this.#displayFirstDayOfWeek(dateStartOfMonth) + 1;
-      i < this.#getDatesOfMonth(month, year).length;
-      i++
-    ) {
-      if (i < 10) {
-        if (this.#getDayName(month, year, i) === "Saturday") {
-          datesOfMonthInString += ` ${i}` + "  " + "\n";
-        } else {
-          datesOfMonthInString += ` ${i}` + " ";
-        }
-      } else {
-        if (this.#getDayName(month, year, i) === "Saturday") {
-          datesOfMonthInString += `${i}` + " " + "\n";
-        } else {
-          datesOfMonthInString += `${i}` + " ";
-        }
-      }
-    }
-    console.log(`${datesOfMonthInString}`);
+    const startOfMonth = DateTime.local(year, month,1);
+    const firstWeekDay = startOfMonth.weekday;
+    this.#displayFirstWeek(firstWeekDay);
   }
 
-  #displayFirstDayOfWeek(dateStartOfMonth) {
-    switch (dateStartOfMonth) {
-      case "Sunday":
-        console.log(" 1  2  3  4  5  6  7");
-        return 7;
-      case "Monday":
-        console.log("    1  2  3  4  5  6");
-        return 6;
-      case "Tuesday":
-        console.log("       1  2  3  4  5");
-        return 5;
-      case "Wednesday":
-        console.log("          1  2  3  4");
-        return 4;
-      case "Thursday":
-        console.log("             1  2  3");
-        return 3;
-      case "Friday":
-        console.log("                1  2");
-        return 2;
-      case "Saturday":
-        console.log("                   1");
-        return 1;
-      default:
-        break;
+  #displayFirstWeek(firstWeekDay) {
+    const startIndex = this.#getStartIndexForFirstWeekDay(firstWeekDay);
+    let dates = "";
+    for (let i = 0; i < startIndex; i++) {
+        dates += "   ";
     }
+    for (let i = 1; i <= 7 - startIndex; i++) {
+        dates += ' ' + i + " ";
+    }
+    console.log(dates);
   }
 
   #getDatesOfMonth(month, year) {
@@ -98,9 +63,12 @@ class MyCalendar {
     return new Intl.DateTimeFormat("ja-JP", { month: "long" }).format(date);
   }
 
-  #getDayName(month, year, day) {
-    const date = new Date(year, month - 1, day);
-    return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
+  #getStartIndexForFirstWeekDay(firstWeekDay){
+    if(firstWeekDay === 7){
+      return 0;
+    }else {
+      return firstWeekDay;
+    }
   }
 }
 
