d0b137fa89677253b571ab2884a4b8859b75b40d
kazuma-naka <kazuma.naka.01@gmail.com>
2024-07-21 14:20:01 -0400

    コールバック形式をPromise でラップする。 prototype を使用する

--- 03.asynchronous/database.js
+++ 03.asynchronous/database.js
@@ -60,10 +60,6 @@ database.run(createtableSQL, (err) => {
                 database.run(dropTableSQL, (err) => {
                   if (err) return console.error(err);
                   console.log("テーブルを削除しました。");
-                  database.close((err) => {
-                    if (err) return console.error(err);
-                    console.log("データベースをクローズしました。");
-                  });
                 });
               },
             );
@@ -115,10 +111,6 @@ try {
                   database.run(dropTableSQL, (err) => {
                     if (err) return console.error(err);
                     console.log("テーブルを削除しました。");
-                    database.close((err) => {
-                      if (err) return console.error(err);
-                      console.log("データベースをクローズしました。");
-                    });
                   });
                 },
               );
@@ -131,3 +123,113 @@ try {
 } catch (err) {
   console.error(err);
 }
+
+await timers.setTimeout(2000);
+
+/* Promise start */
+const originalRun = sqlite3.Database.prototype.run;
+const originalEach = sqlite3.Database.prototype.each;
+const originalClose = sqlite3.Database.prototype.close;
+
+sqlite3.Database.prototype.run = function (sqlQuery, params = []) {
+  return new Promise((resolve, reject) => {
+    if (typeof params === "function" || typeof params === "undefined") {
+      params = [];
+    }
+    originalRun.call(this, sqlQuery, params, function (err) {
+      if (err) reject(err);
+      else {
+        if (sqlQuery === createtableSQL) console.log("Books のテーブルを作成");
+        else if (sqlQuery === insertSQL)
+          console.log(`${params} が挿入されました。\nrowid: ${this.lastID}`);
+        else if (sqlQuery === dropTableSQL)
+          console.log("テーブルを削除しました。");
+        else console.log("不正な SQL クエリです");
+        resolve();
+      }
+    });
+  });
+};
+
+sqlite3.Database.prototype.each = function (sqlQuery) {
+  return new Promise((resolve, reject) => {
+    originalEach.call(this, sqlQuery, function (err, row) {
+      if (err) reject(err);
+      else console.log(`${row.id}: ${row.title}`);
+    });
+    resolve();
+  });
+};
+
+sqlite3.Database.prototype.close = function () {
+  return new Promise((resolve, reject) => {
+    originalClose.call(this, function (err) {
+      if (err) reject(err);
+      else {
+        console.log("データベースをクローズしました。");
+        resolve();
+      }
+    });
+  });
+};
+
+database
+  .run(createtableSQL)
+  .then(() => database.run(insertSQL, [titleNames[0]]))
+  .then(() => database.run(insertSQL, [titleNames[1]]))
+  .then(() => database.run(insertSQL, [titleNames[2]]))
+  .then(() => database.run(insertSQL, [titleNames[3]]))
+  .then(() => database.run(insertSQL, [titleNames[4]]))
+  .then(() => database.each(selectSQL))
+  .then(() => database.run(dropTableSQL))
+  .then(() => database.close())
+  .catch((err) => console.error(err));
+
+// database.run(createtableSQL, (err) => {
+//   if (err) return console.error(err);
+//   console.log("Books のテーブルを作成");
+//   database.run(insertSQL, [titleNames[0]], function (err) {
+//     if (err) return console.error(err);
+//     console.log(`${titleNames[0]} が挿入されました。\nrowid: ${this.lastID}`);
+//     database.run(insertSQL, [titleNames[1]], function (err) {
+//       if (err) return console.error(err);
+//       console.log(`${titleNames[1]} が挿入されました。\nrowid: ${this.lastID}`);
+//       database.run(insertSQL, [titleNames[2]], function (err) {
+//         if (err) return console.error(err);
+//         console.log(
+//           `${titleNames[2]} が挿入されました。\nrowid: ${this.lastID}`,
+//         );
+//         database.run(insertSQL, [titleNames[3]], function (err) {
+//           if (err) return console.error(err);
+//           console.log(
+//             `${titleNames[3]} が挿入されました。\nrowid: ${this.lastID}`,
+//           );
+//           database.run(insertSQL, [titleNames[4]], function (err) {
+//             if (err) return console.error(err);
+//             console.log(
+//               `${titleNames[4]} が挿入されました。\nrowid: ${this.lastID}`,
+//             );
+//             database.each(
+//               selectSQL,
+//               (err, row) => {
+//                 if (err) return console.error(err);
+//                 console.log(`${row.id}: ${row.title}`);
+//               },
+//               (err) => {
+//                 if (err) return console.error(err);
+//                 database.run(dropTableSQL, (err) => {
+//                   if (err) return console.error(err);
+//                   console.log("テーブルを削除しました。");
+//                   database.close((err) => {
+//                     if (err) return console.error(err);
+//                     console.log("データベースをクローズしました。");
+//                   });
+//                 });
+//               },
+//             );
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
