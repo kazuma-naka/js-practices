311cad09faf74c721a052f02afb3744ddc42081a
kazuma-naka <kazuma.naka.01@gmail.com>
2024-07-21 05:35:33 -0400

    SQL の構文にエラーを交えてコールバックで実装する

--- 03.asynchronous/database.js
+++ 03.asynchronous/database.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
 import sqlite3 from "sqlite3";
+import timers from "timers/promises";
 
 const database = new sqlite3.Database(":memory:");
 const createtableSQL = `
@@ -13,6 +14,8 @@ const createtableSQL = `
 const insertSQL = `INSERT INTO books (title) VALUES (?)`;
 const selectSQL = "SELECT id, title FROM books";
 const dropTableSQL = `DROP TABLE IF EXISTS books`;
+const insertSQLError = `INSERT INTO boooks (title) VALUES (?)`;
+const selectSQLError = "SELECT id, tile FROM books";
 const titleNames = [
   "To Kill a Mockingbird",
   "1984",
@@ -21,7 +24,7 @@ const titleNames = [
   "The Catcher in the Rye",
 ];
 
-/* callback starts */
+/* callback start */
 database.run(createtableSQL, (err) => {
   if (err) return console.error(err);
   console.log("Books のテーブルを作成");
@@ -70,3 +73,61 @@ database.run(createtableSQL, (err) => {
     });
   });
 });
+
+await timers.setTimeout(2000);
+
+/* callback with error start */
+try {
+  database.run(createtableSQL, (err) => {
+    if (err) return console.error(err);
+    console.log("Books のテーブルを作成");
+    database.run(insertSQLError, [titleNames[0]], function (err) {
+      if (err) return console.error(err);
+      console.log(`${titleNames[0]} が挿入されました。\nrowid: ${this.lastID}`);
+      database.run(insertSQLError, [titleNames[1]], function (err) {
+        if (err) return console.error(err);
+        console.log(
+          `${titleNames[1]} が挿入されました。\nrowid: ${this.lastID}`,
+        );
+        database.run(insertSQLError, [titleNames[2]], function (err) {
+          if (err) return console.error(err);
+          console.log(
+            `${titleNames[2]} が挿入されました。\nrowid: ${this.lastID}`,
+          );
+          database.run(insertSQLError, [titleNames[3]], function (err) {
+            if (err) return console.error(err);
+            console.log(
+              `${titleNames[3]} が挿入されました。\nrowid: ${this.lastID}`,
+            );
+            database.run(insertSQLError, [titleNames[4]], function (err) {
+              if (err) return console.error(err);
+              console.log(
+                `${titleNames[4]} が挿入されました。\nrowid: ${this.lastID}`,
+              );
+              database.each(
+                selectSQLError,
+                (err, row) => {
+                  if (err) return console.error(err);
+                  console.log(`${row.id}: ${row.title}`);
+                },
+                (err) => {
+                  if (err) return console.error(err);
+                  database.run(dropTableSQL, (err) => {
+                    if (err) return console.error(err);
+                    console.log("テーブルを削除しました。");
+                    database.close((err) => {
+                      if (err) return console.error(err);
+                      console.log("データベースをクローズしました。");
+                    });
+                  });
+                },
+              );
+            });
+          });
+        });
+      });
+    });
+  });
+} catch (err) {
+  console.error(err);
+}
